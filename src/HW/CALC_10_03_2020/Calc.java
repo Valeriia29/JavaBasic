e1b85f3272c2fc1e9c8b411af1d2ad53a350093b
vkonovki <Valeriia.Konovkina@t-systems.com>
2020-04-15 19:22:42 +0200

    remove fish

---
+++ src/HW/CALC_10_03_2020/Calc.java
@@ -0,0 +1,21 @@
+package HW.CALC_10_03_2020;
+
+public class Calc {
+    public static double SUM(double var1,double var2)
+    {
+        return var1+var2;
+    }
+    public static double Minus(double var3,double var4){
+
+        return var3-var4;
+    }
+    public static double multiplier (double var3,double var4){
+
+        return var3*var4;
+    }
+    public static double Division (double var3,double var4){
+
+        return var3/var4;
+    }
+
+}
---
+++ src/HW/CALC_10_03_2020/main_calc.java
@@ -0,0 +1,21 @@
+package HW.CALC_10_03_2020;
+
+import java.util.Scanner;
+
+public class main_calc {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите a");
+        double  a = scanner.nextDouble();
+        System.out.println("Введите b");
+        double b = scanner.nextDouble();
+        double c= Calc.SUM(a,b);
+        System.out.println("The result of the addition is:  " + c);
+        System.out.println("The result of the subtraction is:  " + Calc.Minus(a,b));
+        System.out.println("The result of the multiplication is:  " + Calc.multiplier(a,b));
+        System.out.println("The result of the division is:  " + Calc.Division(a,b));
+
+
+    }
+}
